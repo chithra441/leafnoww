81c6b6244555639fb2e630fdea13be0c19193d2d
Ankit Kumar Sinha <ankitsinha111200@gmail.com>
2022-06-22 12:19:11 +0530

    commit

--- servicess.php
+++ servicess.php
@@ -19,7 +19,7 @@
       href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap"
       rel="stylesheet"
     />
-        <link rel="stylesheet" href="service.css" />
+        <link rel="stylesheet" href="./css/service.css" />
 	<!-- Stylesheets -->
 	<link rel="stylesheet" href="css/bootstrap.min.css"/>
 	<link rel="stylesheet" href="css/font-awesome.min.css"/>
@@ -31,7 +31,7 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 	
-	<link rel="stylesheet" type="text/css" href="Styles.css">
+	<!-- <link rel="stylesheet" type="text/css" href="Styles.css"> -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
 
